005731c0d04d536d9b60a6ef8b59b32af742065d
iiiiirisa <tusen.takk39@gmail.com>
2025-01-23 20:35:15 +0900

    【自己修正】フロントエンド、正常に動作OK

--- src/app/_components/Input.tsx
+++ src/app/_components/Input.tsx
@@ -1,19 +1,25 @@
 "use client";
 
-//Inputコンポーネントの型定義（InputProps）の型定義
+import React, { forwardRef } from "react";
+
+// Inputコンポーネントの型定義（InputProps）の型定義
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 //`HTMLInputElement`が持つすべての属性を含む`React.InputHTMLAttributes`を拡張
 //`<input>タグで使用できるすべてのプロパティ（例えばtype`、`name`、`value`、`onChange`など）をこのコンポーネントでも使用できる
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-//Inputのコンポーネント
-export const Input: React.FC<InputProps> = (props) => {
+// Inputのコンポーネント
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   return (
     <input
       {...props}
+      ref={ref} // refをここで受け取る
       className={
         "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-300 focus:border-green-300 block w-full p-2.5"
       }
     />
   );
-};
+});
+
+// React.forwardRefを使用する場合、displayNameを設定するとデバッグが容易になる
+Input.displayName = "Input";
--- src/app/dashboard/habit/new/page.tsx
+++ src/app/dashboard/habit/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { supabase } from "@/untils/supabase";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { CreateHabitRequestBody } from "@/app/_types/Habit/PostRequest";
 import { Label } from "@/app/_components/Label";
 import { Input } from "@/app/_components/Input";
@@ -9,11 +9,13 @@ import { Button } from "@/app/_components/Button";
 import { Footer } from "@/app/_components/Footer";
 import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
 import { useForm } from "react-hook-form";
+import { useRouteGuard } from "../../_hooks/useRouteGuard";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
-  const [name, setName] = useState("");
-  const [supplementaryDescription, setSupplementaryDescription] = useState("");
+  useRouteGuard(); // ログイン状態を確認
   const { token } = useSupabaseSession();
+  const router = useRouter();
 
   useEffect(() => {
     // Supabaseから現在のユーザー情報を取得
@@ -21,20 +23,24 @@ export default function Page() {
       const { error } = await supabase.auth.getUser();
 
       if (error) {
+        router.replace("/login");
         console.error("Error fetching user:", error.message);
       }
     };
 
     getUserInfo();
-  }, []);
+  }, [router]);
 
   // useFormを使用してフォームの状態を管理
   const {
+    register,
+    handleSubmit,
     formState: { isSubmitting },
-  } = useForm();
+    reset,
+  } = useForm<CreateHabitRequestBody>();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit = async (data: CreateHabitRequestBody) => {
+    console.log("Submitting form with data:", data);
 
     if (!token) {
       alert("ユーザーが認証されていません。");
@@ -42,59 +48,54 @@ export default function Page() {
     }
 
     try {
-      const body: CreateHabitRequestBody = {
-        name,
-        supplementaryDescription,
-      };
       const response = await fetch("/api/dashboard/habit/new", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: token,
         },
-        body: JSON.stringify(body), // 直接bodyを送信
+        body: JSON.stringify(data), // 直接bodyを送信
       });
-
       if (!response.ok) {
         const errorData = await response.json();
-        throw new Error(errorData.message || "サーバーエラーが発生しました。");
-      }
 
-      setName("");
-      setSupplementaryDescription("");
-      alert("習慣を登録しました。");
+        if (response.status === 403) {
+          alert(errorData.message);
+          router.replace("/login");
+        } else {
+          throw new Error(errorData.message);
+        }
+      } else {
+        reset();
+        alert("習慣を登録しました。");
+      }
     } catch (error) {
       console.error("Error submitting form", error);
       alert("エラーが発生しました。もう一度お試しください。");
     }
   };
-
   return (
     <div className="flex justify-center pt-[240px] px-4">
       <div className="w-full  max-w-lg">
-        <form onSubmit={handleSubmit} className="space-y-16">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-16">
           <div>
             <Label htmlFor="habitName">
               身に付けたい習慣（1日5分でできる内容）
             </Label>
             <Input
               type="text"
-              name="habitName"
+              {...register("name", { required: true })}
               id="habitName"
               required
-              onChange={(e) => setName(e.target.value)}
-              value={name}
               disabled={isSubmitting} //送信中には入力やボタンを無効化する
             />
           </div>
           <div>
             <Label htmlFor="supplementaryDescription">補足（あれば）</Label>
             <textarea
-              name="supplementaryDescription"
+              {...register("supplementaryDescription")}
               id="supplementaryDescription"
               className="w-full p-2 border border-gray-300 rounded"
-              onChange={(e) => setSupplementaryDescription(e.target.value)}
-              value={supplementaryDescription}
               disabled={isSubmitting} //送信中には入力やボタンを無効化する
             />
           </div>
