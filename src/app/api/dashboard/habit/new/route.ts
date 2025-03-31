2290c85a2da323892d18868ca7d033ce1d8030fe
iiiiirisa <tusen.takk39@gmail.com>
2025-03-31 22:46:09 +0900

    習慣＿新規登録の認証処理修正

--- src/app/api/dashboard/habit/new/route.ts
+++ src/app/api/dashboard/habit/new/route.ts
@@ -1,31 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { CreateHabitRequestBody } from "@/app/_types/Habit/PostRequest";
-import { supabase } from "@/_untils/supabase";
+import { authenticateUser } from "@/app/_components/Authentication";
 
 const prisma = new PrismaClient();
 
 export const POST = async (request: NextRequest) => {
-  const token = request.headers.get("Authorization") ?? "";
-  console.log(token);
+  // 認証処理
+  //authenticateUser関数にリクエストを渡して認証を実行
+  const authResult = await authenticateUser(request);
 
-  // Supabaseに対してtokenを送る
-  const { data, error } = await supabase.auth.getUser(token);
-  console.log(data);
-  console.log(error);
-
-  // トークンが無効または期限切れの場合、エラーを吐くように変更
-  if (error) {
-    return NextResponse.json(
-      {
-        status: "error",
-        message: "認証トークンが無効です。再ログインしてください。",
-      },
-      { status: 403 }
-    );
+  // NextResponseが返ってきた場合はエラー
+  //(判定したいオブジェクト instanceof オブジェクト名称)：判定したいオブジェクトの種類が instanceof の後ろに記載したオブジェクト名称と一致する場合はtrue、不一致の場合はfalseを返却
+  if (authResult instanceof NextResponse) {
+    return authResult;
   }
 
-  const supabaseId = data.user?.id; // SupabaseのユーザーIDを取得
+  //認証が成功した場合、authResultからユーザー情報を分割代入
+  const { user } = authResult;
+
+  const supabaseId = user.id; // SupabaseのユーザーIDを取得
   console.log(supabaseId);
 
   if (!supabaseId) {
@@ -38,21 +32,21 @@ export const POST = async (request: NextRequest) => {
     );
   }
 
-  const existingUser = await prisma.user.findUnique({
-    where: { supabaseId: supabaseId }, // Supabase IDで検索
-  });
-
-  if (!existingUser) {
-    return NextResponse.json(
-      {
-        status: "error",
-        message: "指定されたユーザーが存在しません。",
-      },
-      { status: 403 }
-    );
-  }
-
   try {
+    const existingUser = await prisma.user.findUnique({
+      where: { supabaseId: supabaseId }, // Supabase IDで検索
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        {
+          status: "error",
+          message: "指定されたユーザーが存在しません。",
+        },
+        { status: 403 }
+      );
+    }
+
     const body: CreateHabitRequestBody = await request.json();
     console.log(body);
 
