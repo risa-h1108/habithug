7f0b892d2ec1a0b42658339dc4ef2e87ceb56d4e
iiiiirisa <tusen.takk39@gmail.com>
2025-04-20 15:08:06 +0900

    型の修正

--- src/app/_types/Confirm/HistoryData.ts
+++ src/app/_types/Confirm/HistoryData.ts
@@ -3,6 +3,6 @@ import { Diary } from "@prisma/client";
 export type HistoryData = {
   id: string;
   date: string;
-  reflection: string;
+  reflection: string[]; //最低3つ以上の自分を褒める内容を個別に取得する為、[]を追加
   diary: Diary;
 };
